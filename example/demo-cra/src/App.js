b221e62107a7c2e74094f7c6cf840ffab73d5f9b
taeeun kim <kimtaeeeny@gmail.com>
2020-10-11 22:12:47 +0900

    [Chore] add demo pages

---
+++ example/demo-cra/src/App.js
@@ -0,0 +1,20 @@
+import React, { useEffect } from "react";
+import { Chart } from "interactive-chart";
+import data from "./data.json";
+import styled from "styled-components";
+
+const ChartComp = styled.div`
+  width: 700px;
+  border: 2px solid black;
+  background-color: #fff;
+  padding: 12px;
+`;
+
+const App = () => {
+  useEffect(() => {
+    new Chart({ selector: "line-chart", chartTitle: "Accountbook Line Chart", data });
+  }, []);
+  return <ChartComp id="line-chart"></ChartComp>;
+};
+
+export default App;
